bbda0133a3c7cd7da531f2bdfc3e0b875317eadd
Kisik Misik <700mmr@gmail.com>
2020-01-27 04:16:52 +0200

    added html for main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,53 @@
 "use strict";
-var gulp      = require('gulp'), // Подключаем Gulp
-    less        = require('gulp-less'), //Подключаем Sass пакет,
-    browserSync = require('browser-sync'); // Подключаем Browser Sync
 
-gulp.task('less', function(){ // Создаем таск Sass
-    return gulp.src('app/less/**/*.less') // Берем источник
-        .pipe(less()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("dist/css"))
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
-        },
-        notify: false // Отключаем уведомления
+gulp.task("html", function () {
+  return gulp.src("app/**/*.html")
+  .pipe(gulp.dest("dist"))
+});
+
+gulp.task("fonts", function () {
+  return gulp.src(["app/fonts/**/*.{woff,woff2}"])
+    .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("app/img/**/*.{png,jpg,svg}")
+  .pipe(gulp.dest("dist/img"))
+});
+
+gulp.task("js", function () {
+  return gulp.src("app/js/**/*.js")
+  .pipe(gulp.dest("dist/js"))
+});
+
+gulp.task("server", function () {
+
+    server.init({
+        server: "app/"
     });
+
+  gulp.watch("app/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("app/*.html").on('change', server.reload);
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/less/**/*.less', gulp.parallel('less')); // Наблюдение за sass файлами
-});
-gulp.task('default', gulp.parallel('less', 'browser-sync', 'watch'));
-
+gulp.task("start", gulp.series("server", "css"));
